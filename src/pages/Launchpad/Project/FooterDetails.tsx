685720662c7781fb8fdf0a2409d30d9d8cfe5693
kojiadrianojr <iisparkplugiiog@yahoo.com>
2021-07-01 22:30:14 +0800

    Project footer with navigation

--- src/pages/Launchpad/Project/FooterDetails.tsx
+++ src/pages/Launchpad/Project/FooterDetails.tsx
@@ -1,5 +1,5 @@
 import { Flex, Button, Heading, Text } from '@sparkpointio/sparkswap-uikit';
-import React, { useContext } from 'react';
+import React, { useContext, useState, useCallback } from 'react';
 import styled, { ThemeContext } from 'styled-components';
 import { CustomThemeContext } from 'ThemeContext';
 
@@ -9,20 +9,20 @@ const Container = styled(Flex)`
     height: auto;
 `;
 
-const NavOptions = styled(Button)<{activeIndex: boolean}>`
+const NavOptions = styled(Button)<{ activeIndex: boolean }>`
     background-color: transparent;
-    color: ${({theme, activeIndex}) => activeIndex? theme.colors.text: theme.colors.textSubtle};
-    border-bottom: ${({theme, activeIndex}) => activeIndex && `3px solid ${theme.colors.primary}`};
-`
+    color: ${({ theme, activeIndex }) => (activeIndex ? theme.colors.text : theme.colors.textSubtle)};
+    border-bottom: ${({ theme, activeIndex }) => activeIndex && `3px solid ${theme.colors.primary}`};
+`;
 
-const FooterNav = ({ index = 0}: { index?: number }) => {
+const FooterNav = ({ activeIndex = 0, handleClick}: { activeIndex?: number; handleClick: (i:number) => void }) => {
     const theme = useContext(ThemeContext);
     const customTheme = useContext(CustomThemeContext);
     return (
-        <Flex alignItems="center" style={{margin: '10px 10px 0px 10px', padding: '20px 20px 0px 20px'}}>
-           <NavOptions activeIndex={index === 0}>Project Details</NavOptions>
-           <NavOptions activeIndex={index === 1}>Schedule</NavOptions>
-           <NavOptions activeIndex={index === 2}>Your Allocation</NavOptions>
+        <Flex alignItems="center" style={{ margin: '10px 10px 0px 10px', padding: '20px 20px 0px 20px' }}>
+            <NavOptions activeIndex={activeIndex === 0} onClick={() => handleClick(0)}>Project Details</NavOptions>
+            <NavOptions activeIndex={activeIndex === 1} onClick={() => handleClick(1)}>Schedule</NavOptions>
+            <NavOptions activeIndex={activeIndex === 2} onClick={() => handleClick(2)}>Your Allocation</NavOptions>
         </Flex>
     );
 };
@@ -30,88 +30,66 @@ const FooterNav = ({ index = 0}: { index?: number }) => {
 const FooterDetails: React.FC = () => {
     const theme = useContext(ThemeContext);
     const customTheme = useContext(CustomThemeContext);
+    const [ activeIndex , setActive ] = useState<number>(0)
+
+    const handleClick = useCallback((i) => {
+        setActive(i)
+    }, [])
+    
 
     return (
         <Container
             style={{ backgroundColor: theme.isDark ? customTheme.customColors?.BG1 : customTheme.customColors?.BG2 }}
             flexDirection="column"
         >
-        <div style={{marginBottom: '10px', borderBottom: `1px solid ${theme.colors.primary}`}}>
-            <FooterNav />
-        </div>
-            <Flex padding="20px" margin="20px" justifyContent="space-between">
+            <div style={{ marginBottom: '10px', borderBottom: `1px solid ${theme.colors.primary}` }}>
+                <FooterNav handleClick={handleClick} activeIndex={activeIndex} />
+            </div>
+            { activeIndex === 0 && <Flex padding="20px" margin="20px" justifyContent="space-between">
                 <Flex flex="1" flexDirection="column" marginRight="10px">
-                    <Heading margin="10px 0px 30px 0" bold>Pool Information</Heading>
+                    <Heading margin="10px 0px 30px 0" bold>
+                        Pool Information
+                    </Heading>
                     <Flex justifyContent="space-between">
-                        <Text>
-                            Opens
-                        </Text> 
-                        <Text color="textSubtle">
-                            06/12/2021 7:00 UTC
-                        </Text>
+                        <Text>Opens</Text>
+                        <Text color="textSubtle">06/12/2021 7:00 UTC</Text>
                     </Flex>
                     <Flex justifyContent="space-between">
-                        <Text>
-                            Closes
-                        </Text> 
-                        <Text color="textSubtle">
-                            06/31/2021 7:00 UTC
-                        </Text>
+                        <Text>Closes</Text>
+                        <Text color="textSubtle">06/31/2021 7:00 UTC</Text>
                     </Flex>
                     <Flex justifyContent="space-between">
-                        <Text>
-                            Cap
-                        </Text> 
-                        <Text color="textSubtle">
-                            150 BNB
-                        </Text>
+                        <Text>Cap</Text>
+                        <Text color="textSubtle">150 BNB</Text>
                     </Flex>
                     <Flex justifyContent="space-between">
-                        <Text>
-                            Total Users Participated
-                        </Text> 
-                        <Text color="textSubtle">
-                            123456789
-                        </Text>
+                        <Text>Total Users Participated</Text>
+                        <Text color="textSubtle">123456789</Text>
                     </Flex>
                     <Flex justifyContent="space-between">
-                        <Text>
-                            Total Funds Swapped
-                        </Text> 
-                        <Text color="textSubtle">
-                            118 BNB
-                        </Text>
+                        <Text>Total Funds Swapped</Text>
+                        <Text color="textSubtle">118 BNB</Text>
                     </Flex>
                 </Flex>
                 <Flex flex="1" marginLeft="10px" flexDirection="column">
-                    <Heading margin="10px 0px 30px 0px" bold> Token Information</Heading>
+                    <Heading margin="10px 0px 30px 0px" bold>
+                        {' '}
+                        Token Information
+                    </Heading>
                     <Flex justifyContent="space-between">
-                        <Text>
-                            Name
-                        </Text> 
-                        <Text color="textSubtle">
-                            OWNLY
-                        </Text>
+                        <Text>Name</Text>
+                        <Text color="textSubtle">OWNLY</Text>
                     </Flex>
                     <Flex justifyContent="space-between">
-                        <Text>
-                            Token Symbol
-                        </Text> 
-                        <Text color="textSubtle">
-                            OWN
-                        </Text>
+                        <Text>Token Symbol</Text>
+                        <Text color="textSubtle">OWN</Text>
                     </Flex>
                     <Flex justifyContent="space-between">
-                        <Text>
-                            Total Supply
-                        </Text> 
-                        <Text color="textSubtle">
-                            165,000,000.00
-                        </Text>
+                        <Text>Total Supply</Text>
+                        <Text color="textSubtle">165,000,000.00</Text>
                     </Flex>
-
                 </Flex>
-            </Flex>
+            </Flex>}
         </Container>
     );
 };
