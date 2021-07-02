e9b4da03db2d82992a46578e723d7f3c06490596
kojiadrianojr <iisparkplugiiog@yahoo.com>
2021-07-02 13:15:26 +0800

    Update Data types

--- src/config/constants/type.ts
+++ src/config/constants/type.ts
@@ -1,7 +1,10 @@
 export interface IProjects {
     title: string;
+    symbol: string;
     image: string;
     desc: string;
+    longDesc: string;
+    price: string;
     progress: number;
     totalRaise: number;
     ownSale: number;
@@ -10,6 +13,22 @@ export interface IProjects {
     wallpaperBg?: string;
     status?: string;
     address?: string;
+}
+
+export interface IUserAccount {
+    address: string;
+    allocation: string;
+    maxSwap: string;
+}
+
+export interface IPoolInformation {
+    open: string;
+    closes: string;
+    cap: string;
+    totalUserParticipated: string;
+    totalFundsSwapped: string;
+    buyingToken: string;
+    projectAddress: string;
 }
 
 interface ListType {
