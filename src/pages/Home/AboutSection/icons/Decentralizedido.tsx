834e3bebaa96677e2c996288bb373667667fdb02
Mat <mativanarquero@gmail.com>
2021-07-21 14:42:50 +0800

    Render about icons depending on theme context

--- src/pages/Home/AboutSection/icons/Decentralizedido.tsx
+++ src/pages/Home/AboutSection/icons/Decentralizedido.tsx
@@ -2,13 +2,15 @@ import React, { SVGAttributes, useContext } from "react";
 import { ThemeContext } from "styled-components";
 import { SvgProps } from 'components/SvgIcon/types';
 import { ReactComponent as DecentralizedidoIcon } from 'assets/About/Decentralizedido.svg';
+import { ReactComponent as DecentralizedidolightIcon } from 'assets/About/Decentralizedidolight.svg';
 import SvgIcon from "components/SvgIcon";
 
 const Icon: React.FC<SvgProps> = (props) => {
- 
-  return (
-    <SvgIcon width={117} Icon={DecentralizedidoIcon} />
-  )
+    const theme = useContext(ThemeContext);
+
+    return (
+      <SvgIcon width={118.5} Icon={theme.isDark? DecentralizedidoIcon : DecentralizedidolightIcon } />      
+    )
 };
 
 export default Icon;
--- src/pages/Home/AboutSection/icons/Rapidlaunches.tsx
+++ src/pages/Home/AboutSection/icons/Rapidlaunches.tsx
@@ -2,13 +2,15 @@ import React, { SVGAttributes, useContext } from "react";
 import { ThemeContext } from "styled-components";
 import { SvgProps } from 'components/SvgIcon/types';
 import { ReactComponent as RapidlaunchesIcon } from 'assets/About/Rapidlaunches.svg';
+import { ReactComponent as RapidlauncheslightIcon } from 'assets/About/Rapidlaunches_light.svg';
 import SvgIcon from "components/SvgIcon";
 
 const Icon: React.FC<SvgProps> = (props) => {
- 
-  return (
-    <SvgIcon width={126} Icon={RapidlaunchesIcon} />
-  )
+    const theme = useContext(ThemeContext);
+
+    return (
+      <SvgIcon width={128} Icon={theme.isDark? RapidlaunchesIcon : RapidlauncheslightIcon} />
+    )
 };
 
 export default Icon;
