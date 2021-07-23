44a99ff2e6e273f5aa7bbb91f92433c722bce439
Mat <mativanarquero@gmail.com>
2021-07-23 21:36:59 +0800

    Update mobile responsive support for Iphone 6, 7, 8

--- src/pages/Home/HeaderSection/styled.tsx
+++ src/pages/Home/HeaderSection/styled.tsx
@@ -69,7 +69,15 @@ export const StyledLink = styled(Link)`
     
     @media (max-width: 770px ) {
         width: 100%;
-        height: 6vh;
+        height: 6.8vh;
+    }
+    @media (max-width: 375px) {
+        width: 100%;
+        height: 7.2vh;
+    }
+    @media (max-width: 320px ) {
+        width: 100%;
+        height: 8.1vh;
     }
     
 `
