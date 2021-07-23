4fd81a7f9507e4f11fbd91784c0f1c5af5becbcc
Mat <mativanarquero@gmail.com>
2021-07-23 15:13:04 +0800

    Update arrow icon function for purchase modal

--- src/assets/icons/Arrow.tsx
+++ src/assets/icons/Arrow.tsx
@@ -5,15 +5,15 @@ function Icon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="120.072"
-      height="80.364"
-      viewBox="0 0 46.667 60.001"
+      width="26.667"
+      height="36.667"
+      viewBox="0 0 26.667 36.667"
     >
       <path
         fill="#1b60a3"
-        d="M44.167 51.2V27.834H37.5V51.2h-10l13.334 13.3 13.333-13.3zM20.834 4.5L7.5 17.8h10v23.367h6.667V17.8h10z"
+        d="M44.167,51.2V27.834H37.5V51.2h-10L40.834,64.5,54.167,51.2Z"
         data-name="Icon material-swap-vert"
-        transform="translate(-7.5 -4.5)"
+        transform="translate(-27.5 -27.834)"
       />
     </svg>
   );
