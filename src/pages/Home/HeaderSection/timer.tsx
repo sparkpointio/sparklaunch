b4d64ab574e48561520a3c5b375d2512f810a2be
Mat <mativanarquero@gmail.com>
2021-07-25 16:16:27 +0800

    Add countdown timer

---
+++ src/pages/Home/HeaderSection/timer.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+function Timer() {
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`${year}-07-27`) - +new Date();
+    // let difference = new Date("Jul 27, 2021 17:00:00").getTime();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [year] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  // return Ownly is Live after countdown expires
+  return (
+    <div>
+      {timerComponents.length ? timerComponents : <span>Ownly is Live!</span>}
+    </div>
+  );
+}
+
+export default Timer;
