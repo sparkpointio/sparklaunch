680d896af66adab3e8a3f2544fa510dcff1d819d
Medard Mandane <medardm13@gmail.com>
2021-07-25 17:57:20 +0800

    Updates

--- static/js/10.ca06611c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpsparklaunch=this.webpackJsonpsparklaunch||[]).push([[10],{457:function(n,c){},458:function(n,c){},467:function(n,c){},468:function(n,c){},488:function(n,c){},491:function(n,c){},492:function(n,c){},497:function(n,c){},501:function(n,c){},575:function(n,c){}}]);
-//# sourceMappingURL=10.ca06611c.chunk.js.map
