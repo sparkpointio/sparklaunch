fdf36422c0f6b3b0e28031a82f5d31509cc5dca2
Aldrick Bonaobra <git@aldrickb.com>
2021-07-27 19:15:54 +0800

    Update open and close time

--- src/config/dummy-data/Pool.ts
+++ src/config/dummy-data/Pool.ts
@@ -3,8 +3,8 @@ import { IPoolInformation } from "config/constants/type"
  const PoolList:IPoolInformation[] = [
     {
         projectAddress: '0x001',
-        open: '07/27/2021 9:00 UTC',
-        close: '08/27/2021 9:00 UTC',
+        open: '07/27/2021 11:00 GMT',
+        close: '07/28/2021 11:00 GMT',
         cap: 45000,
         totalUserParticipated: 123456789,
         totalFundsSwapped: 0,
