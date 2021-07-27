3fff7a73b2d697f343b48c579f9f8918ab96e194
Aldrick Bonaobra <git@aldrickb.com>
2021-07-27 18:27:32 +0800

    Update token address and logo

--- src/config/dummy-data/Tokens.ts
+++ src/config/dummy-data/Tokens.ts
@@ -7,7 +7,7 @@ const TokenList:ITokens[] = [
         address: "0xae13d989dac2f0debff460ac112a837c89baa7cd",
         chainId: 97,
         decimals: 18,
-        logoURI: "/images/coins/BUSD.png"
+        logoURI: "/images/icons/WBNB.png"
     },
     {
         name: "BNB Token",
@@ -15,7 +15,7 @@ const TokenList:ITokens[] = [
         address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
         chainId: 56,
         decimals: 18,
-        logoURI: "/images/coins/SRK.png"
+        logoURI: "/images/icons/WBNB.png"
     },
     {
         name: "YLNWO",
@@ -23,15 +23,15 @@ const TokenList:ITokens[] = [
         address: "0xe09B8661D80CF24dB230A167969d18B94a5a3266",
         chainId: 97,
         decimals: 18,
-        logoURI: "/images/coins/SRK.png"
+        logoURI: "/images/icons/own.png"
     },
     {
-        name: "YLNWO",
-        symbol: "NWO",
-        address: "0x446494947a1865d872ab43d383b8e264b4cf1bf0",
+        name: "OWNLY",
+        symbol: "OWN",
+        address: "0x7665cb7b0d01df1c9f9b9cc66019f00abd6959ba",
         chainId: 56,
         decimals: 18,
-        logoURI: "/images/coins/SRK.png"
+        logoURI: "/images/icons/own.png"
     }
 ]
 
