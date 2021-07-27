5c2557970f6f0339747e4e4bbc30019226874aca
Mat <mativanarquero@gmail.com>
2021-07-27 13:04:32 +0800

    Import price per bnb api to pruchase modal

--- src/components/Modals/PurchaseModal.tsx
+++ src/components/Modals/PurchaseModal.tsx
@@ -10,11 +10,14 @@ import CurrencyInputPanel from 'components/CurrencyInputPanel';
 import { Button, Flex, Heading, Modal, Text } from '@sparkpointio/sparkswap-uikit';
 import styled, { ThemeContext } from 'styled-components';
 import { SmallstyledImage } from 'pages/Launchpad/components/styled';
+import getPriceBnb from 'hooks/getPriceBnb'
 import { useOwnlyLaunchpad } from '../../hooks/useContracts';
 import { BNB, OWN } from '../../config';
 import useActiveWeb3React from '../../hooks/useActiveWeb3React';
 import { expandValue } from '../../utils';
 import { getAccountDetailsLaunchPad } from '../../utils/contractHelpers';
+import pricePerBnb from 'hooks/getPriceBnb';
+
 
 
 interface AppProps {
@@ -246,6 +249,7 @@ const PurchaseModal: React.FC<AppProps> = ({ onDismiss, address }) => {
                     currency={project}
                     remainingSupply={remainingPurchasable.toExact()}
                 />
+                
                 <ActionDiv>
                     <Button onClick={handleBuy} fullWidth>
                         Swap
