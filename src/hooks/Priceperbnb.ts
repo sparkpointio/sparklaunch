732f03830c020cae6f9b22907d23cb2e63215136
Mat <mativanarquero@gmail.com>
2021-07-27 15:48:37 +0800

    Update fetching bnb api

--- src/hooks/Priceperbnb.ts
+++ src/hooks/Priceperbnb.ts
@@ -1,44 +1,27 @@
-// Fetch api price per BNB
-
 import React, { useState, useEffect } from "react";
 
 
-const fetchData = () => {
-    return fetch("https://api.sparkswap.info/api/assets")
-        .then((response) => response.json())
-        .then((data)=> console.log(data));}
-        // {data.0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c[0].BNB}
-
-        export default fetchData
-
-
-/*
-export default function Priceperbnb() {
+export default function PricePerBnb() {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     useEffect(() => {
-        fetch("https://api.sparkswap.info/api/assets")
+        fetch("https://wallet.sparkpoint.io/api/v2/wallet/conversionRates")
             .then((response) => {
                 if (response.ok) {
                     return response.json();
                 }
                 throw response;
             })
-            .catch((error) => {
-                console.error("Error fetching data: ", error);
-                setError(error);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
+           
     }, []);
     if (loading) return "Loading...";
     if (error) return "Error!";
-
+    
     return (
         // {data.0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c[0].BNB}
+        data.last_updated[0].bnb
         
     );
 }
-*/
+
