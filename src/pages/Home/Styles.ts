f23e63d182d31e9535511474b42a6fe903f77c9b
Mat <mativanarquero@gmail.com>
2021-08-03 14:38:27 +0800

    Fix back to top button positioning

--- src/pages/Home/Styles.ts
+++ src/pages/Home/Styles.ts
@@ -10,6 +10,10 @@ font-size: 3rem;
 z-index: 1;
 cursor: pointer;
 color: #006bf8;
+@media (max-width: 700px){
+    left: 89%;
+    bottom: 98px;
+  }
 @media (max-width: 600px){
     left: 83%;
     bottom: 98px;
@@ -17,5 +21,9 @@ color: #006bf8;
   @media (max-width: 530px){
     left: 83%;
     bottom: 85px;
+  }
+  @media (max-width: 500px){
+    left: 83%;
+    bottom: 110px;
   }
 `
