77110b8dce3da7b15d6f79493d8a43967ef2300c
Aldrick Bonaobra <git@aldrickb.com>
2021-10-31 21:55:27 +0800

    Update test whitelist

--- src/config/data/Accounts.ts
+++ src/config/data/Accounts.ts
@@ -31,6 +31,21 @@ const AccountList: IAccountType[] = [
             }, 
             {
                 address: "0x7C11371ca161276DE5340A08B3f204F4c6c66e3a"
+            },
+            {
+                address: "0x645E8537A6252Aac5BEb1CeDE014E6b21894242F"
+            },
+            {
+                address: "0x8A672715e042f6e9d9B25C2ce9F84210e8206EF1"
+            },
+            {
+                address: "0xA5920b19E4793cF6AF2EE001df3e7317BF1cE42A"
+            },
+            {
+                address: "0x221Ea53305e826DD03Cc4D2b763b526cb33243A9"
+            },
+            {
+                address: "0xcE84500ad8411b14e5DA048adCdF46284660a0AA"
             }
         ]
     }, {
