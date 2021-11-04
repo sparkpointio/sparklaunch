9dc7c60c578d6107bcbefb42cb3024513c6ff0a7
Aldrick Bonaobra <git@aldrickb.com>
2021-11-04 20:36:16 +0800

    Fix OWN contract order

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -15,11 +15,11 @@
 
 export default {
     ownlyLaunchPad$1: {
-        56: '0x75f5696c02978fd345Ad81AE89F81402aFc7342F',
+        56: '0xf5dC1243Fd734A4A4583a651c3100A2347e1A2d5', 
         97: '0xe99646c31060991166c37220ce8ab5453e573dae',
     },
     ownlyLaunchPad$2: {
-        56: '0xf5dC1243Fd734A4A4583a651c3100A2347e1A2d5',
+        56: '0x75f5696c02978fd345Ad81AE89F81402aFc7342F',
         97: '0x6171ef8577601bfebc36db044f53a5d874536d0e',
     },
     outRaceIDO$1: {
