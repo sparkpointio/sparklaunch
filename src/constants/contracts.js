38873fbefefca13f40f1cc591c47af0fb7567696
Aldrick Bonaobra <git@aldrickb.com>
2021-11-04 20:20:32 +0800

    Fix OWN project

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -18,10 +18,10 @@ export default {
         56: '0x75f5696c02978fd345Ad81AE89F81402aFc7342F',
         97: '0xe99646c31060991166c37220ce8ab5453e573dae',
     },
-    // ownlyLaunchPad$2: {
-    //     56: '0xf5dC1243Fd734A4A4583a651c3100A2347e1A2d5',
-    //     97: '0x6171ef8577601bfebc36db044f53a5d874536d0e',
-    // },
+    ownlyLaunchPad$2: {
+        56: '0xf5dC1243Fd734A4A4583a651c3100A2347e1A2d5',
+        97: '0x6171ef8577601bfebc36db044f53a5d874536d0e',
+    },
     outRaceIDO$1: {
         56: '0x75f5696c02978fd345Ad81AE89F81402aFc7342F',
         97: '0x66bC605D68b471A3Fd8724137439D5857c3B1Caa',
