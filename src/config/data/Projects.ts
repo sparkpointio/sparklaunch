03c59de34ad52f8d86bf25e68fd8803b02f9c6dc
Medard Mandane <medardm13@gmail.com>
2021-11-22 21:02:53 +0800

    Update start date of outrace

--- src/config/data/Projects.ts
+++ src/config/data/Projects.ts
@@ -77,7 +77,7 @@ const ProjectList: Array<IProjects> = [
         ownSale: 0,
         wallpaperBg: 'oreBG6.png',
         status: 'upcoming',
-        startDate: epochToDate(1637586000),
+        startDate: epochToDate(1637587800),
         endDate: epochToDate(1637758800),
         // hasDateEnded: dateEnded(1637596800),
         claimable: false,
